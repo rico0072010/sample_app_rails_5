045c09fb4f2f21f3db8f1cc4cf63c19f4b394042
rico0072010 <danildolin96@gmail.com>
2020-04-05 21:54:46 +0500

    Finish user signup

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module SampleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+      "<div class=\"field_with_form_errors\">#{html_tag}</div>".html_safe
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
