f539bf9b7e4d758d3f2bac4bce5bd8c7f7059b6e
rico0072010 <danildolin96@gmail.com>
2020-04-13 20:45:08 +0500

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ ruby '2.7.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.1'
 gem 'bootstrap-sass', '3.4.1'
+gem 'faker', '2.11.0'
+gem 'will_paginate', '3.3.0'
+gem 'bootstrap-will_paginate', '1.0.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,19 +1,31 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
 
-  test "layout links" do
+  test 'layout links' do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', users_path, count: 0
     get contact_path
-    assert_select "title", full_title("Contact")
+    assert_select 'title', full_title('Contact')
     get root_path
-    assert_select "a[href=?]", signup_path
+    assert_select 'a[href=?]', signup_path
+    assert_select 'a[href=?]', login_path
     get signup_path
-    assert_select "title", full_title("Sign up")
+    assert_select 'title', full_title('Sign up')
+    get signup_path
+    log_in_as(@user)
+    get root_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', edit_user_path(@user)
   end
 end
